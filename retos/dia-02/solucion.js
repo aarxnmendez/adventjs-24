15502ac7b5872ef029624648161e6e6e9c69c4dc
Aaron Mendez <aarxnmendezz@gmail.com>
2024-12-02 09:58:06 +0100

    Update dia-02 solucion.js

--- retos/dia-02/solucion.js
+++ retos/dia-02/solucion.js
@@ -1,5 +1,6 @@
 function createFrame(names) {
     let longest = 0;
+    // Find the longest name
     for (let i = 0; i < names.length; i++) {
         if (names[i].length > longest) {
             longest = names[i].length;
@@ -7,11 +8,14 @@ function createFrame(names) {
     }
 
     let frame = '';
+
+    // Create the top of the frame
     for (let i = 0; i < longest + 4; i++) {
         frame += '*';
     }
     frame += '\n';
 
+    // Create the body of the frame
     for (let i = 0; i < names.length; i++) {
         frame += '* ' + names[i];
         for (let j = 0; j < longest - names[i].length; j++) {
@@ -20,8 +24,10 @@ function createFrame(names) {
         frame += ' *\n';
     }
 
+    // Create the bottom of the frame
     for (let i = 0; i < longest + 4; i++) {
         frame += '*';
     }
+    
     return frame;
 }
